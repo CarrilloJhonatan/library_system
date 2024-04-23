39025d3e11eb86a9b138c11c7b2dec4c22490b7a
JHONATAN STEVEN CARRILLO MEJIA <89527312+CarrilloJhonatan@users.noreply.github.com>
2024-04-23 18:59:29 -0500

    Mas o miercoles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,158 +1,157 @@
 import {
-    RiNotification3Line,
-    RiArrowDownSLine,
-    RiSettings3Line,
-    RiLogoutCircleRLine,
-    RiThumbUpLine,
-    RiChat3Line,
-  } from "react-icons/ri";
-  import "../App.css";
-  import { Menu, MenuItem, MenuButton } from "@szhsin/react-menu";
-  import "@szhsin/react-menu/dist/index.css";
-  import "@szhsin/react-menu/dist/transitions/slide.css";
-  import { Link } from "react-router-dom";
-  
-  const Header = () => {
-    return (
-      <header className="h-[7vh] md:h-[10vh] color-secundario border-b border-secondary-100 p-8 flex drop-shadow-md items-center justify-end">
-        <nav className="flex items-center gap-2">
-          <Menu
-            menuButton={
-              <MenuButton className="relative hover:bg-secondary-100 p-2 rounded-lg transition-colors">
-                <RiNotification3Line />
-                <span className="absolute -top-0.5 right-0 bg-primary py-0.5 px-[5px] box-content text-black rounded-full text-[8px] font-bold">
-                  2
-                </span>
-              </MenuButton>
-            }
-            align="end"
-            arrow
-            transition
-            arrowClassName="bg-secondary-100"
-            menuClassName="bg-secondary-100 p-4"
-          >
-            <h1 className="text-gray-700 text-center font-medium">
-              Notificaciones (2)
-            </h1>
-            <hr className="my-6 border-gray-500" />
-            <MenuItem className="p-0 hover:bg-transparent">
-              <Link
-                to="/"
-                className="text-gray-700 flex flex-1 items-center gap-4 py-2 px-4 hover:bg-secondary-900 transition-colors rounded-lg"
-              >
-                <img
-                  src="https://img.freepik.com/foto-gratis/feliz-optimista-guapo-gerente-ventas-latina-apuntando-lado-mirando-camara_1262-12679.jpg"
-                  className="w-8 h-8 object-cover rounded-full"
-                />
-                <div className="text-sm flex flex-col">
-                  <div className="flex items-center justify-between gap-4">
-                    <span>Jorge Luis Trejo</span>{" "}
-                    <span className="text-[8px] tex">21/10/2022</span>
-                  </div>
-                  <p className="text-gray-700 text-xs">
-                    Lorem ipsum dolor sit amet...
-                  </p>
+  RiNotification3Line,
+  RiArrowDownSLine,
+  RiSettings3Line,
+  RiLogoutCircleRLine,
+  RiThumbUpLine,
+  RiChat3Line,
+} from "react-icons/ri";
+import "../App.css";
+import { Menu, MenuItem, MenuButton } from "@szhsin/react-menu";
+import "@szhsin/react-menu/dist/index.css";
+import "@szhsin/react-menu/dist/transitions/slide.css";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <header className="h-[7vh] md:h-[10vh] bg-white border-b border-secondary-100 p-8 flex drop-shadow-md items-center justify-end">
+      <nav className="flex items-center gap-2">
+        <Menu
+          menuButton={
+            <MenuButton className="relative hover:bg-secondary-100 p-2 rounded-lg transition-colors">
+              <RiNotification3Line />
+              <span className="absolute -top-0.5 right-0 bg-primary py-0.5 px-[5px] box-content text-black rounded-full text-[8px] font-bold">
+                2
+              </span>
+            </MenuButton>
+          }
+          align="end"
+          arrow
+          transition
+          arrowClassName="bg-secondary-100"
+          menuClassName="bg-secondary-100 p-4"
+        >
+          <h1 className="text-gray-700 text-center font-medium">
+            Notificaciones (2)
+          </h1>
+          <hr className="my-6 border-gray-500" />
+          <MenuItem className="p-0 hover:bg-transparent">
+            <Link
+              to="/"
+              className="text-gray-700 flex flex-1 items-center gap-4 py-2 px-4 hover:bg-secondary-900 transition-colors rounded-lg"
+            >
+              <img
+                src="https://img.freepik.com/foto-gratis/feliz-optimista-guapo-gerente-ventas-latina-apuntando-lado-mirando-camara_1262-12679.jpg"
+                className="w-8 h-8 object-cover rounded-full"
+              />
+              <div className="text-sm flex flex-col">
+                <div className="flex items-center justify-between gap-4">
+                  <span>Jorge Luis Trejo</span>{" "}
+                  <span className="text-[8px] tex">21/10/2022</span>
                 </div>
-              </Link>
-            </MenuItem>
-            <MenuItem className="p-0 hover:bg-transparent">
-              <Link
-                to="/"
-                className="text-gray-700 flex flex-1 items-center gap-4 py-2 px-4 hover:bg-secondary-900 transition-colors rounded-lg"
-              >
-                <RiThumbUpLine className="p-2 bg-blue-200 text-blue-700 box-content rounded-full" />
-                <div className="text-sm flex flex-col">
-                  <div className="flex items-center justify-between gap-4">
-                    <span>Nuevo like</span>{" "}
-                    <span className="text-[8px]">21/10/2022</span>
-                  </div>
-                  <p className="text-gray-700 text-xs">
-                    A Jorge Trejo le gusta tu pub...
-                  </p>
+                <p className="text-gray-700 text-xs">
+                  Lorem ipsum dolor sit amet...
+                </p>
+              </div>
+            </Link>
+          </MenuItem>
+          <MenuItem className="p-0 hover:bg-transparent">
+            <Link
+              to="/"
+              className="text-gray-700 flex flex-1 items-center gap-4 py-2 px-4 hover:bg-secondary-900 transition-colors rounded-lg"
+            >
+              <RiThumbUpLine className="p-2 bg-blue-200 text-blue-700 box-content rounded-full" />
+              <div className="text-sm flex flex-col">
+                <div className="flex items-center justify-between gap-4">
+                  <span>Nuevo like</span>{" "}
+                  <span className="text-[8px]">21/10/2022</span>
                 </div>
-              </Link>
-            </MenuItem>
-            <MenuItem className="p-0 hover:bg-transparent">
-              <Link
-                to="/"
-                className="text-gray-700 flex flex-1 items-center gap-4 py-2 px-4 hover:bg-secondary-900 transition-colors rounded-lg"
-              >
-                <RiChat3Line className="p-2 bg-yellow-200 text-yellow-700 box-content rounded-full" />
-                <div className="text-sm flex flex-col">
-                  <div className="flex items-center justify-between gap-4">
-                    <span>Nuevo comentario</span>{" "}
-                    <span className="text-[8px]">21/10/2022</span>
-                  </div>
-                  <p className="text-gray-700 text-xs">
-                    Jorge Trejo ha comentado tu...
-                  </p>
+                <p className="text-gray-700 text-xs">
+                  A Jorge Trejo le gusta tu pub...
+                </p>
+              </div>
+            </Link>
+          </MenuItem>
+          <MenuItem className="p-0 hover:bg-transparent">
+            <Link
+              to="/"
+              className="text-gray-700 flex flex-1 items-center gap-4 py-2 px-4 hover:bg-secondary-900 transition-colors rounded-lg"
+            >
+              <RiChat3Line className="p-2 bg-yellow-200 text-yellow-700 box-content rounded-full" />
+              <div className="text-sm flex flex-col">
+                <div className="flex items-center justify-between gap-4">
+                  <span>Nuevo comentario</span>{" "}
+                  <span className="text-[8px]">21/10/2022</span>
                 </div>
-              </Link>
-            </MenuItem>
-            <hr className="my-6 border-gray-500" />
-            <MenuItem className="p-0 hover:bg-transparent flex justify-center cursor-default">
-              <Link
-                to="/"
-                className="text-gray-700 text-sm hover:text-white transition-colors"
-              >
-                Todas las notificaciones
-              </Link>
-            </MenuItem>
-          </Menu>
-          <Menu
-            menuButton={
-              <MenuButton className="flex items-center gap-x-2 hover:bg-secondary-100 p-2 rounded-lg transition-colors">
-                <img
-                  src="https://img.freepik.com/foto-gratis/feliz-optimista-guapo-gerente-ventas-latina-apuntando-lado-mirando-camara_1262-12679.jpg"
-                  className="w-6 h-6 object-cover rounded-full"
-                />
-                <span>Luis Fernando</span>
-                <RiArrowDownSLine />
-              </MenuButton>
-            }
-            align="end"
-            arrow
-            arrowClassName="bg-secondary-100"
-            transition
-            menuClassName="bg-secondary-100 p-4"
-          >
-            <MenuItem className="p-0 hover:bg-transparent">
-              <Link
-                to="/perfil"
-                className="rounded-lg transition-colors text-gray-700 hover:bg-secondary-900 flex items-center gap-x-4 py-2 px-6 flex-1"
-              >
-                <img
-                  src="https://img.freepik.com/foto-gratis/feliz-optimista-guapo-gerente-ventas-latina-apuntando-lado-mirando-camara_1262-12679.jpg"
-                  className="w-8 h-8 object-cover rounded-full"
-                />
-                <div className="flex flex-col text-sm">
-                  <span className="text-sm">Luis Fernando</span>
-                  <span className="text-xs text-gray-500">fernado@gmail.com</span>
-                </div>
-              </Link>
-            </MenuItem>
-            <hr className="my-4 border-gray-500" />
-            <MenuItem className="p-0 hover:bg-transparent">
-              <Link
-                to="/configuracion"
-                className="rounded-lg transition-colors text-gray-700 hover:bg-secondary-900 flex items-center gap-x-4 py-2 px-6 flex-1"
-              >
-                <RiSettings3Line /> Configuración
-              </Link>
-            </MenuItem>
-            <MenuItem className="p-0 hover:bg-transparent">
-              <Link
-                to="/cerrar-sesion"
-                className="rounded-lg transition-colors text-gray-700 hover:bg-secondary-900 flex items-center gap-x-4 py-2 px-6 flex-1"
-              >
-                <RiLogoutCircleRLine /> Cerrar sesión
-              </Link>
-            </MenuItem>
-          </Menu>
-        </nav>
-      </header>
-    );
-  };
-  
-  export default Header;
-  
+                <p className="text-gray-700 text-xs">
+                  Jorge Trejo ha comentado tu...
+                </p>
+              </div>
+            </Link>
+          </MenuItem>
+          <hr className="my-6 border-gray-500" />
+          <MenuItem className="p-0 hover:bg-transparent flex justify-center cursor-default">
+            <Link
+              to="/"
+              className="text-gray-700 text-sm hover:text-white transition-colors"
+            >
+              Todas las notificaciones
+            </Link>
+          </MenuItem>
+        </Menu>
+        <Menu
+          menuButton={
+            <MenuButton className="flex items-center gap-x-2 hover:bg-secondary-100 p-2 rounded-lg transition-colors">
+              <img
+                src="https://img.freepik.com/foto-gratis/feliz-optimista-guapo-gerente-ventas-latina-apuntando-lado-mirando-camara_1262-12679.jpg"
+                className="w-6 h-6 object-cover rounded-full"
+              />
+              <span>Luis Fernando</span>
+              <RiArrowDownSLine />
+            </MenuButton>
+          }
+          align="end"
+          arrow
+          arrowClassName="bg-secondary-100"
+          transition
+          menuClassName="bg-secondary-100 p-4"
+        >
+          <MenuItem className="p-0 hover:bg-transparent">
+            <Link
+              to="/perfil"
+              className="rounded-lg transition-colors text-gray-700 hover:bg-secondary-900 flex items-center gap-x-4 py-2 px-6 flex-1"
+            >
+              <img
+                src="https://img.freepik.com/foto-gratis/feliz-optimista-guapo-gerente-ventas-latina-apuntando-lado-mirando-camara_1262-12679.jpg"
+                className="w-8 h-8 object-cover rounded-full"
+              />
+              <div className="flex flex-col text-sm">
+                <span className="text-sm">Luis Fernando</span>
+                <span className="text-xs text-gray-500">fernado@gmail.com</span>
+              </div>
+            </Link>
+          </MenuItem>
+          <hr className="my-4 border-gray-500" />
+          <MenuItem className="p-0 hover:bg-transparent">
+            <Link
+              to="/configuracion"
+              className="rounded-lg transition-colors text-gray-700 hover:bg-secondary-900 flex items-center gap-x-4 py-2 px-6 flex-1"
+            >
+              <RiSettings3Line /> Configuración
+            </Link>
+          </MenuItem>
+          <MenuItem className="p-0 hover:bg-transparent">
+            <Link
+              to="/cerrar-sesion"
+              className="rounded-lg transition-colors text-gray-700 hover:bg-secondary-900 flex items-center gap-x-4 py-2 px-6 flex-1"
+            >
+              <RiLogoutCircleRLine /> Cerrar sesión
+            </Link>
+          </MenuItem>
+        </Menu>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -59,7 +59,7 @@ const Login = () => {
         </div>
         <div className="my-14">
           <p className="text-center relative text-gray-500 bg-gray-100 before:max-w-[50px] md:before:max-w-[120px] before:w-full before:-left-[60px] md:before:-left-[140px] before:h-[1px] before:bg-current before:absolute before:top-[50%] after:max-w-[50px] md:after:max-w-[120px] after:w-full after:h-[1px] after:bg-current after:absolute after:top-[50%] after:-right-[60px] md:after:-right-[140px]">
-            Log in with your email address
+            Log in with your username
           </p>
         </div>
         {/* Formulario de inicio de sesión */}
